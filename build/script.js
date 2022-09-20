46cf34b3d2ae08b55d1713b90b597fc18202c55f
LunaDuram <lucineiaduram@gmail.com>
2022-09-20 15:15:14 -0300

    exemplo

--- build/script.js
+++ build/script.js
@@ -5,4 +5,12 @@ var duck = new Duck_1.Duck('Patolino', 3, 50, 2, true, 'preto c/verde');
 function takeOff(animal) {
     animal.fly();
 }
+function Swim(animal) {
+    animal.swim();
+}
+function Walk(animal) {
+    animal.walk();
+}
 takeOff(duck);
+Swim(duck);
+Walk(duck);
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,20 @@
 import {Duck} from "./Duck";
 import { ICanFly } from "./ICanFly";
+import { ICanSwim } from "./ICanSwim";
+import { ICanWalk } from "./ICanWalk";
 
 const duck = new Duck('Patolino', 3, 50, 2, true, 'preto c/verde');
 
 function takeOff(animal: ICanFly){
     animal.fly();
 }
-
+function Swim(animal: ICanSwim){
+    animal.swim();
+}
+function Walk(animal: ICanWalk){
+    animal.walk();
+}
 
 takeOff(duck);
+Swim(duck);
+Walk(duck);
